6e8c7c8a203da13d6e3e956b4299f8db573aa6c8
Sonakshi <2201730005@krmu.edu.in>
2025-07-20 22:45:43 +0530

    deployment

--- run_st_IBM.bat
+++ run_st_IBM.bat
@@ -2,4 +2,4 @@
 cd /d "%~dp0"
 C:\Users\Sonakshi\AppData\Local\Programs\Python\Python313\python.exe -m streamlit run "C:\Users\Sonakshi\OneDrive\Attachments\Desktop\employee_dashboard-site\salary_explorer_dashboard.py"
 
-pause
+pause 
