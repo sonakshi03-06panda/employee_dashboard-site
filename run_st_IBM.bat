b5e63873e7e73474035d98f3441864b32478c9b6
Sonakshi <2201730005@krmu.edu.in>
2025-07-21 14:40:46 +0530

    delete

--- run_st_IBM.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d "%~dp0"
-C:\Users\Sonakshi\AppData\Local\Programs\Python\Python313\python.exe -m streamlit run "C:\Users\Sonakshi\OneDrive\Attachments\Desktop\PROJECTS_apps\employee_dashboard-site\salary_explorer_dash.py"
-
-pause 
